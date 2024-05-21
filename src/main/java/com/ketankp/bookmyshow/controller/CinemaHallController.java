8220a9a9c2b2e65f2778d6d84a213b176bda86cf
ketankp <ketankp06@gmail.com>
2024-05-21 21:20:19 +0530

    Get and create API for CinemaHall Model

--- src/main/java/com/ketankp/bookmyshow/controller/CinemaHallController.java
+++ src/main/java/com/ketankp/bookmyshow/controller/CinemaHallController.java
@@ -6,7 +6,6 @@ import com.ketankp.bookmyshow.service.CinemaHallService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- src/main/java/com/ketankp/bookmyshow/service/impl/CinemaHallServiceImpl.java
+++ src/main/java/com/ketankp/bookmyshow/service/impl/CinemaHallServiceImpl.java
@@ -6,30 +6,31 @@ import com.ketankp.bookmyshow.model.CinemaHall;
 import com.ketankp.bookmyshow.repository.CinemaHallRepository;
 import com.ketankp.bookmyshow.service.CinemaHallService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.lang.reflect.Type;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
+@Log4j2
 public class CinemaHallServiceImpl implements CinemaHallService {
 
     private final CinemaHallRepository cinemaHallRepository;
 
     @Override
     public List<CinemaHallResponseDto> getAllCinemaHall() {
+        log.info("CinemaHallServiceImpl:getAllCinemaHall method started");
         ModelMapper mapper = new ModelMapper();
         return mapper.map(cinemaHallRepository.findAll(),new TypeToken<List<CinemaHallResponseDto>>(){}.getType());
     }
 
     @Override
     public CinemaHallResponseDto createCinemaHall(CinemaHallRequestDto cinemaHallRequestDto) {
+        log.info("CinemaHallServiceImpl:createCinemaHall method started");
         ModelMapper modelMapper = new ModelMapper();
         CinemaHall cinemaHall = modelMapper.map(cinemaHallRequestDto,CinemaHall.class);
         cinemaHallRepository.save(cinemaHall);
